627b0f1066827c00b70ab4569ebc8b4b7bffdcf1
beyourahi <beyourahi@gmail.com>
2023-11-26 18:12:41 +0600

    file upload dropzone

---
+++ app/dashboard/[fileId]/page.tsx
@@ -0,0 +1,41 @@
+// import ChatWrapper from "components/chat/ChatWrapper";
+// import PdfRenderer from "components/PdfRenderer";
+// import { getUserSubscriptionPlan } from "lib/stripe";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+import { db } from "db";
+import { PdfRenderer } from "components/PdfRenderer";
+import { ChatWrapper } from "components/chat/ChatWrapper";
+
+const Page = async ({ params: { fileId } }: { params: { fileId: string } }) => {
+    const user = await getKindeServerSession().getUser();
+
+    if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileId}`);
+
+    const file = await db.file.findFirst({ where: { id: fileId, userId: user.id } });
+
+    if (!file) notFound();
+
+    // const plan = await getUserSubscriptionPlan();
+
+    return (
+        <div className="flex h-[calc(100vh-3.5rem)] flex-1 flex-col justify-between">
+            <div className="max-w-8xl mx-auto w-full grow lg:flex xl:px-2">
+                {/* Left sidebar & main wrapper */}
+                <div className="flex-1 xl:flex">
+                    <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+                        {/* Main area */}
+                        <PdfRenderer />
+                    </div>
+                </div>
+
+                <div className="flex-[0.75] shrink-0 border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+                    {/* <ChatWrapper isSubscribed={plan.isSubscribed} fileId={file.id} /> */}
+                    ChatWrapper
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
